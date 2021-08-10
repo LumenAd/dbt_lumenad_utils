42949eedba38d9681729655227def5b125f2c7ee
Jordan Fowler <jfowler@lumenad.com>
2021-08-10 13:57:41 -0600

    Lowercase all s3 path parts for consistency.

--- macros/unload_records.sql
+++ macros/unload_records.sql
@@ -2,6 +2,6 @@
     {% set time_since = "dateadd(" ~ time_part ~ ", " ~ time_value ~ ", " ~ time_field ~ ")" %}
     {% set fields = dbt_utils.star(from=model) %}
     {% set query = "SELECT " ~ fields ~ ", " ~ lumenad_utils.unload_partition_keys(time_field) ~ " FROM " ~ model ~ " WHERE " ~ time_field ~ " >= " ~ time_since %}
-    {% set s3_path = "@" ~ stage_name ~ "/" ~ model.database ~ '/' ~ model.schema ~ '/' ~ model.table %}
+    {% set s3_path = "@" ~ stage_name ~ "/" ~ model.database|lower ~ '/' ~ model.schema|lower ~ '/' ~ model.table|lower %}
     {% do lumenad_utils.unload_to_s3(query, s3_path) %}
 {% endmacro %}
