75e413cebedc2f088cd1f40ff1ae336a726fe475
Jordan Fowler <jfowler@lumenad.com>
2021-08-11 20:48:21 -0600

    Fully qualified names for all resources.

--- macros/unload_records.sql
+++ macros/unload_records.sql
@@ -2,6 +2,7 @@
     {% set time_since = "dateadd(" ~ time_part ~ ", " ~ time_value ~ ", " ~ time_field ~ ")" %}
     {% set fields = dbt_utils.star(from=model) %}
     {% set query = "SELECT " ~ fields ~ ", " ~ lumenad_utils.unload_partition_keys(time_field) ~ " FROM " ~ model ~ " WHERE " ~ time_field ~ " >= " ~ time_since %}
-    {% set s3_path = "@" ~ stage_name ~ "/" ~ model.database|lower ~ '/' ~ model.schema|lower ~ '/' ~ model.table|lower %}
-    {% do lumenad_utils.unload_to_s3(query, s3_path) %}
+    {% set s3_path = "@" ~  model.database|lower ~ '.' ~ model.schema|lower ~ '.' ~ stage_name ~ "/" ~ model.database|lower ~ '/' ~ model.schema|lower ~ '/' ~ model.table|lower %}
+    {% set file_format = model.database|lower ~ '.' ~ model.schema|lower ~ '.parquet_file_type' %}
+    {% do lumenad_utils.unload_to_s3(query, s3_path, file_format) %}
 {% endmacro %}
--- macros/unload_to_s3.sql
+++ macros/unload_to_s3.sql
@@ -1,8 +1,8 @@
-{% macro unload_to_s3(query, s3_path) %}
+{% macro unload_to_s3(query, s3_path, file_format) %}
     {% set unload_query %}
         COPY INTO {{ s3_path }} FROM ({{ query }})
         PARTITION BY (concat('year=' || year, '/month=' || month, '/day=' || day, '/hour=' || hour))
-        FILE_FORMAT = (format_name = parquet_file_type)
+        FILE_FORMAT = (format_name = {{ file_format }})
         HEADER = true
     {% endset %}
     {% do run_query(unload_query) %}
